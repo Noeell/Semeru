463ab25baa29e553f8c14679c3af62a27d900f14
JJoeyy <joeru@bluewin.ch>
2021-08-27 11:24:06 +0200

    login

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,44 @@
-import {Button} from "react-bootstrap";
+import Firebase from "firebase";
+import {useState} from "react";
+import {Link, useHistory} from "react-router-dom";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB1AltKxep7brHa87pM7x8YOS7Oo48AYMQ",
+    authDomain: "localhost",
+    databaseURL: "https://semeru-ef465-default-rtdb.europe-west1.firebasedatabase.app/",
+    storageBucket: "gs://semeru-ef465.appspot.com"
+}
+
+Firebase.initializeApp(firebaseConfig);
 
 export default function Login(){
+
+    const [eMail, setEMail] = useState()
+    const [password, setPassword] = useState()
+
+    const history = useHistory();
+    function buttonPressed() {
+        if (eMail !== null && password !== null) {
+            Firebase.auth()
+                .signInWithEmailAndPassword(eMail, password)
+                .then(() => history.push(''))
+                .catch(error => console.error("could not create user: ", error))
+        }
+    }
+
     return (
         <div>
-            <p>Joey</p>
-            <p>Noel</p>
-            <Button variant={"secondary"}>boo</Button>
+            <h1>Login</h1>
+            <br/>
+            <h4>E-Mail</h4>
+            <input onChange={e => setEMail(e.target.value)}/>
+            <br/>
+            <h4>Passwort</h4>
+            <input type="password" name="password" onChange={e => setPassword(e.target.value)}/>
+            <br/>
+            <button onClick={buttonPressed}>login</button>
+            <br/>
+            <Link to="/about">Sie haben noch kein Konto? Hier registrieren</Link>
         </div>
     );
 }
