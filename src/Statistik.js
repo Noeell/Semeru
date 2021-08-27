bf077d930fc2728d3ccd8c0335608a10bba1472b
Noel <noel.gebhardt@gmail.com>
2021-08-27 13:00:39 +0200

    routing

--- src/Statistik.js
+++ src/Statistik.js
@@ -4,11 +4,18 @@ export default function Statistik() {
     return (
         <div>
             <Container>
+                <br/><br/>
                     <Row>
                         <Col>
                          <h1>Statistik</h1>
                         </Col>
+                        <Col>
+                            <h3></h3>
+                        </Col>
                     </Row>
+                <Row>
+
+                </Row>
             </Container>
         </div>
     )
