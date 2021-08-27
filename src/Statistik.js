65bf8f4dbc8bd4b5f4bea78b075bc60f7b20b51a
Noel <noel.gebhardt@gmail.com>
2021-08-27 11:45:49 +0200

    routing works

--- src/Statistik.js
+++ src/Statistik.js
@@ -1,7 +1,15 @@
+import {Col, Container, Row} from "react-bootstrap";
+
 export default function Statistik() {
     return (
         <div>
-
+            <Container>
+                    <Row>
+                        <Col>
+                         <h1>Statistik</h1>
+                        </Col>
+                    </Row>
+            </Container>
         </div>
     )
 }
