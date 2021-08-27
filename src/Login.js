145034abf582f0cd5bbced0d9c4ba118b8e75e7d
Noel <noel.gebhardt@gmail.com>
2021-08-27 10:44:14 +0200

    first commit semeru

---
+++ src/Login.js
@@ -0,0 +1,10 @@
+import {Button} from "react-bootstrap";
+
+export default function Login(){
+    return (
+        <div>
+            <p>hello</p>
+            <Button variant={"secondary"}>boo</Button>
+        </div>
+    );
+}
