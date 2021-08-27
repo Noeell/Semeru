50c2567277c239cd9767030b69136131a0edb695
JJoeyy <joeru@bluewin.ch>
2021-08-27 11:01:44 +0200

    hallo

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,7 @@ import {Button} from "react-bootstrap";
 export default function Login(){
     return (
         <div>
-            <p>hello</p>
+            <p>Hallo</p>
             <Button variant={"secondary"}>boo</Button>
         </div>
     );
