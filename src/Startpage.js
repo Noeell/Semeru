d29305bce06db74e5236ef2aababfb9b094144a9
Noel <noel.gebhardt@gmail.com>
2021-08-27 13:32:23 +0200

    bootstrap

--- src/Startpage.js
+++ src/Startpage.js
@@ -1,8 +1,22 @@
+import {Col, Container, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
+
 export default function Startpage() {
     return (
         <div>
-            <p>hello</p>
-            <p>hel</p>
+         <Container><br/>
+             <Row>
+                 <Col>
+                     <h1>Time Boxing</h1>
+                 </Col>
+                 <Col>
+
+                 </Col>
+                 <Col>
+                     <Link to="/statistik">Statistik</Link>
+                 </Col>
+             </Row>
+         </Container>
         </div>
     );
 }
--- src/Statistik.js
+++ src/Statistik.js
@@ -1,20 +1,21 @@
 import {Col, Container, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 export default function Statistik() {
     return (
         <div>
             <Container>
-                <br/><br/>
-                    <Row>
-                        <Col>
-                         <h1>Statistik</h1>
-                        </Col>
-                        <Col>
-                            <h3></h3>
-                        </Col>
-                    </Row>
+                <br/>
                 <Row>
+                    <Col>
+                        <h1>Statistik</h1>
+                    </Col>
+                    <Col>
 
+                    </Col>
+                    <Col>
+                        <Link to="/startseite">Time Boxing</Link>
+                    </Col>
                 </Row>
             </Container>
         </div>
