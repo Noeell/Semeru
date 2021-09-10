a09cfb4533863ccbf093c8d7c28d1ce42a39f7a5
Noel <noel.gebhardt@gmail.com>
2021-09-10 09:01:34 +0200

    new database

--- src/Startpage.js
+++ src/Startpage.js
@@ -7,35 +7,98 @@ export default function Startpage() {
     const [allTasks, setAllTasks] = useState()
     const [runningTask, setRunningTask] = useState(0)
 
-
-    Firebase.app().database()
-        .ref(`users/${Firebase.app().auth().currentUser?.uid}`)
-        .get()
-        .then(snapshot => {
+    useEffect(() => {
             Firebase.app().database()
                 .ref(`users/${Firebase.app().auth().currentUser?.uid}`)
-                .update({
-                    eMail: parseInt(snapshot.val()?.eMail) ? parseInt(snapshot.val()?.eMail) : Firebase.app().auth().currentUser.email
+                .get()
+                .then(snapshot => {
+                    Firebase.app().database()
+                        .ref(`users/${Firebase.app().auth().currentUser?.uid}`)
+                        .update({
+                            eMail: parseInt(snapshot.val()?.eMail) ? parseInt(snapshot.val()?.eMail) : Firebase.app().auth().currentUser.email,
+                            day: snapshot.val().day ? snapshot.val().day : 0
+                        })
                 })
-        })
 
-    useEffect(() => {
-        Firebase.app().database()
-            .ref(`users/${Firebase.app().auth().currentUser?.uid}/tasks`)
-            .get()
-            .then(snapshot => {
-                setAllTasks(Object.keys(snapshot.val() || {}))
-            })
+            Firebase.app().database()
+                .ref(`users/${Firebase.app().auth().currentUser?.uid}`)
+                .get()
+                .then(snapshot => {
+                    let updates = false
+                    if (snapshot.val().day !== 0) {
+                        let actualDate = snapshot.val().day.split(".")
+                        let actualDay = actualDate[0]
+                        let actualMonth = actualDate[1]
+                        let actualYear = actualDate[2]
 
-        Firebase.app().database()
-            .ref(`users/${Firebase.app().auth().currentUser?.uid}`)
-            .get()
-            .then(snapshot => {
-                if (runningTask === 0){
-                    setRunningTask(snapshot.val()?.runningTask || null)
-                }
-            })
-    }, [runningTask])
+
+                        if (actualDay !== new Date().getDate()) {
+                            updates = true
+                        } else if (actualMonth !== new Date().getMonth()) {
+                            updates = true
+                        } else if (actualYear !== new Date().getFullYear()) {
+                            updates = true
+                        }
+                    }
+
+                    if (updates === true) {
+                        Firebase.app().database()
+                            .ref(`users/${Firebase.app().auth().currentUser?.uid}/tasks`)
+                            .get()
+                            .then(snapshot => {
+                                const objects = Object.values(snapshot.val() || {});
+
+                                Firebase.app().database()
+                                    .ref(`users/${Firebase.app().auth().currentUser?.uid}/deletedtasks`)
+                                    .get()
+                                    .then(snapshot => {
+                                        const objects2 = Object.values(snapshot.val() || {});
+                                        let list = [...objects, ...objects2]
+
+                                        list.map((item, index)=> {
+                                            Firebase.app().database()
+                                                .ref(`users/${Firebase.app().auth().currentUser?.uid}/${item.name}`)
+                                                .update({
+                                                    tag: "0:0"
+                                                })
+                                        })
+                                        })
+
+
+                            })
+                    }
+                })
+
+            Firebase.app().database()
+                .ref(`users/${Firebase.app().auth().currentUser?.uid}`)
+                .get()
+                .then(snapshot => {
+                    Firebase.app().database()
+                        .ref(`users/${Firebase.app().auth().currentUser?.uid}`)
+                        .update({
+                            day: new Date().getDate() + "." + new Date().getMonth() + "." + new Date().getFullYear()
+                        })
+                })
+
+            Firebase.app().database()
+                .ref(`users/${Firebase.app().auth().currentUser?.uid}/tasks`)
+                .get()
+                .then(snapshot => {
+                    setAllTasks(Object.keys(snapshot.val() || {}))
+                })
+
+            Firebase.app().database()
+                .ref(`users/${Firebase.app().auth().currentUser?.uid}`)
+                .get()
+                .then(snapshot => {
+                    if (runningTask === 0) {
+                        setRunningTask(snapshot.val()?.runningTask || null)
+                    }
+                })
+        }
+        ,
+        [runningTask]
+    )
 
     function startClicked(task) {
         console.log(runningTask)
@@ -73,11 +136,11 @@ export default function Startpage() {
                             .ref(`users/${Firebase.app().auth().currentUser?.uid}/tasks/${runningTask}`)
                             .get()
                             .then(snapshot => {
-                                let hours = Math.floor((Date.now() - snapshot.val().start)/1000/3600)
-                                let minutes = Math.floor((Date.now() - snapshot.val().start)/1000/60 - (Math.floor((Date.now() - snapshot.val().start)/1000/3600)*60))
+                                let hours = Math.floor((Date.now() - snapshot.val().start) / 1000 / 3600)
+                                let minutes = Math.floor((Date.now() - snapshot.val().start) / 1000 / 60 - (Math.floor((Date.now() - snapshot.val().start) / 1000 / 3600) * 60))
                                 let time = hours + ":" + minutes
 
-                                if (snapshot.val().tag === 0){
+                                if (snapshot.val().tag === 0) {
                                     Firebase.app().database()
                                         .ref(`users/${Firebase.app().auth().currentUser?.uid}/tasks/${runningTask}`)
                                         .update({
@@ -86,10 +149,10 @@ export default function Startpage() {
                                 } else {
                                     let oldTime = snapshot.val().tag.split(":")
                                     let newHours = Math.floor((parseInt(oldTime[1]) + minutes) / 60)
-                                    if (parseInt(oldTime[1]) + minutes >= 60){
+                                    if (parseInt(oldTime[1]) + minutes >= 60) {
                                         hours = parseInt(oldTime[0]) + hours + newHours
                                         minutes = (parseInt(oldTime[1]) + minutes) - newHours * 60
-                                    } else{
+                                    } else {
                                         hours = parseInt(oldTime[0]) + hours
                                         minutes = parseInt(oldTime[1]) + minutes
                                     }
@@ -101,11 +164,11 @@ export default function Startpage() {
                                         })
                                 }
 
-                                hours = Math.floor((Date.now() - snapshot.val().start)/1000/3600)
-                                minutes = Math.floor((Date.now() - snapshot.val().start)/1000/60 - (Math.floor((Date.now() - snapshot.val().start)/1000/3600)*60))
+                                hours = Math.floor((Date.now() - snapshot.val().start) / 1000 / 3600)
+                                minutes = Math.floor((Date.now() - snapshot.val().start) / 1000 / 60 - (Math.floor((Date.now() - snapshot.val().start) / 1000 / 3600) * 60))
                                 time = hours + ":" + minutes
 
-                                if (snapshot.val().woche === 0){
+                                if (snapshot.val().woche === 0) {
                                     Firebase.app().database()
                                         .ref(`users/${Firebase.app().auth().currentUser?.uid}/tasks/${runningTask}`)
                                         .update({
@@ -114,10 +177,10 @@ export default function Startpage() {
                                 } else {
                                     let oldTime = snapshot.val().woche.split(":")
                                     let newHours = Math.floor((parseInt(oldTime[1]) + minutes) / 60)
-                                    if (parseInt(oldTime[1]) + minutes >= 60){
+                                    if (parseInt(oldTime[1]) + minutes >= 60) {
                                         hours = parseInt(oldTime[0]) + hours + newHours
                                         minutes = (parseInt(oldTime[1]) + minutes) - newHours * 60
-                                    } else{
+                                    } else {
                                         hours = parseInt(oldTime[0]) + hours
                                         minutes = parseInt(oldTime[1]) + minutes
                                     }
@@ -129,11 +192,11 @@ export default function Startpage() {
                                         })
                                 }
 
-                                hours = Math.floor((Date.now() - snapshot.val().start)/1000/3600)
-                                minutes = Math.floor((Date.now() - snapshot.val().start)/1000/60 - (Math.floor((Date.now() - snapshot.val().start)/1000/3600)*60))
+                                hours = Math.floor((Date.now() - snapshot.val().start) / 1000 / 3600)
+                                minutes = Math.floor((Date.now() - snapshot.val().start) / 1000 / 60 - (Math.floor((Date.now() - snapshot.val().start) / 1000 / 3600) * 60))
                                 time = hours + ":" + minutes
 
-                                if (snapshot.val().monat === 0){
+                                if (snapshot.val().monat === 0) {
                                     Firebase.app().database()
                                         .ref(`users/${Firebase.app().auth().currentUser?.uid}/tasks/${runningTask}`)
                                         .update({
@@ -142,10 +205,10 @@ export default function Startpage() {
                                 } else {
                                     let oldTime = snapshot.val().monat.split(":")
                                     let newHours = Math.floor((parseInt(oldTime[1]) + minutes) / 60)
-                                    if (parseInt(oldTime[1]) + minutes >= 60){
+                                    if (parseInt(oldTime[1]) + minutes >= 60) {
                                         hours = parseInt(oldTime[0]) + hours + newHours
                                         minutes = (parseInt(oldTime[1]) + minutes) - newHours * 60
-                                    } else{
+                                    } else {
                                         hours = parseInt(oldTime[0]) + hours
                                         minutes = parseInt(oldTime[1]) + minutes
                                     }
@@ -157,8 +220,8 @@ export default function Startpage() {
                                         })
                                 }
                             })
-                        console.log(Math.floor((Date.now() - snapshot.val().start)/1000/3600) + ":" +
-                            Math.floor((Date.now() - snapshot.val().start)/1000/60 - (Math.floor((Date.now() - snapshot.val().start)/1000/3600)*60)))
+                        console.log(Math.floor((Date.now() - snapshot.val().start) / 1000 / 3600) + ":" +
+                            Math.floor((Date.now() - snapshot.val().start) / 1000 / 60 - (Math.floor((Date.now() - snapshot.val().start) / 1000 / 3600) * 60)))
                     })
             })
 
@@ -191,10 +254,12 @@ export default function Startpage() {
                         <tr key={index}>
                             <th width={"60%"}>{task}</th>
                             <th>
-                                <Button disabled={runningTask === task} onClick={() => startClicked(task)}>start</Button>
+                                <Button disabled={runningTask === task}
+                                        onClick={() => startClicked(task)}>start</Button>
                             </th>
                             <th>
-                                <Button disabled={runningTask !== task} onClick={() => stopClicked()}>stop</Button>
+                                <Button disabled={runningTask !== task}
+                                        onClick={() => stopClicked()}>stop</Button>
                             </th>
                         </tr>
                     )}
